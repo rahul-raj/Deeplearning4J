a777fcf3c2793c0bccb8a2d62a4a3f5afabf6012
Rahul Raj <omrahulrajcse@gmail.com>
2018-06-05 11:56:55 +0530

    filesplit fix

--- src/main/java/AnimalClassifier.java
+++ src/main/java/AnimalClassifier.java
@@ -3,12 +3,25 @@ import org.datavec.api.io.labels.ParentPathLabelGenerator;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.split.InputSplit;
 import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.FlipImageTransform;
+import org.datavec.image.transform.ImageTransform;
+import org.datavec.image.transform.WarpImageTransform;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AnimalClassifier {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        int channels = 3;
+        int batchSize=10;
 
         File parentDir = new File("C:/Users/Admin/Downloads/imagenet");
         FileSplit fileSplit = new FileSplit(parentDir, NativeImageLoader.ALLOWED_FORMATS,new Random(42));
@@ -19,5 +32,22 @@ public class AnimalClassifier {
         //System.out.println("args = [" + inputSplits[0].length() + "]"+inputSplits[1].length()+" ");
         InputSplit trainData = inputSplits[0];
         InputSplit testData = inputSplits[1];
+
+        ImageTransform transform1 = new FlipImageTransform(new Random(42));
+        ImageTransform transform2 = new FlipImageTransform(new Random(123));
+        ImageTransform transform3 = new WarpImageTransform(new Random(42),42);
+
+        List<ImageTransform> transformList = new ArrayList<>();
+        transformList.add(transform1);
+        transformList.add(transform2);
+        transformList.add(transform3);
+
+        DataNormalization scaler = new ImagePreProcessingScaler(0,1);
+
+
+        ImageRecordReader imageRecordReader = new ImageRecordReader(100,100,channels,parentPathLabelGenerator);
+        imageRecordReader.initialize(trainData,null);
+        DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(imageRecordReader,batchSize);
+
     }
 }

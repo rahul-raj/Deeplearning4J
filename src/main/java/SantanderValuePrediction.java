b901283f1b4e435394bae17f93f1488c6c962f7f
Rahul Raj <omrahulrajcse@gmail.com>
2018-06-22 23:04:42 +0530

    removed unused imports+ santander data pre-process fix without spark code

--- src/main/java/SantanderValuePrediction.java
+++ src/main/java/SantanderValuePrediction.java
@@ -1,27 +1,16 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.transform.TransformProcess;
-import org.datavec.api.transform.analysis.DataAnalysis;
-import org.datavec.api.transform.analysis.columns.ColumnAnalysis;
-import org.datavec.api.transform.analysis.columns.LongAnalysis;
 import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.writable.Writable;
-import org.datavec.spark.transform.AnalyzeSpark;
-import org.datavec.spark.transform.misc.StringToWritablesFunction;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.io.ClassPathResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
 public class SantanderValuePrediction {
 
@@ -30,7 +19,6 @@ public class SantanderValuePrediction {
 
     public static void main(String[] args) throws IOException, InterruptedException, IllegalAccessException, InstantiationException {
 
-        System.setProperty("hadoop.home.dir","C:/Users/Rahul_Raj05/Downloads/winutils-master/winutils-master/hadoop-3.0.0/");
         Schema schema = new Schema.Builder()
                                   .addColumnsString("ID")
                                   .addColumnLong("target")
@@ -43,37 +31,29 @@ public class SantanderValuePrediction {
         recordReader.initialize(new FileSplit(new ClassPathResource("train.csv").getFile()));
         TransformProcessRecordReader transformProcessRecordReader = new TransformProcessRecordReader(recordReader,transformProcess);
 
-
-/*
-        SparkConf sparkConf = new SparkConf();
-        sparkConf.setMaster("local[*]");
-        sparkConf.setAppName("Santander App");
-        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> directory = javaSparkContext.textFile(new ClassPathResource("train.csv").getFile().getParent());
-        JavaRDD<List<Writable>> parsedData = directory.map(new StringToWritablesFunction(new CSVRecordReader()));
-
-        DataAnalysis dataAnalysis = AnalyzeSpark.analyze(schema,parsedData);
-        System.out.println("args = [" + dataAnalysis + "]");
-*/
-       // List<Writable> record = transformProcessRecordReader.next();
-
-        TransformProcessRecordReader temp = transformProcessRecordReader;
         double[] max = new double[4992];
         double[] min = new double[4992];
-        while(temp.hasNext()){
-               List<Writable> record = temp.next();
+        while(transformProcessRecordReader.hasNext()){
+               List<Writable> record = transformProcessRecordReader.next();
                for(int i=1;i<=4991;i++){
                      max[i]=Math.max(max[i],record.get(i).toDouble());
                      min[i]=Math.min(min[i],record.get(i).toDouble());
                }
         }
-        int count=0;
+
+        recordReader=new CSVRecordReader(1,',');
+        recordReader.initialize(new FileSplit(new ClassPathResource("train.csv").getFile()));
+        TransformProcess.Builder builder = new TransformProcess.Builder(schema)
+                                                               .removeColumns("ID");
         for(int i=1;i<=4991;i++){
             if(max[i]==min[i]){
-                count++;
+                builder.removeColumns("col_"+String.valueOf(i));
             }
         }
-        System.out.println("args = [" + count + "]");
+
+
+        transformProcessRecordReader = new TransformProcessRecordReader(recordReader,builder.build());
+        System.out.println("args = [" + transformProcessRecordReader.next().size() + "]");
 
 
 
